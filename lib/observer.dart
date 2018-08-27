855be0608f03f951ef13ca4a46218527e096adeb
Microdog <dlangu0393@gmail.com>
2018-08-27 10:05:06 +0800

    Add UMengAnalyticsRouteObserver

---
+++ lib/observer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'flutter_umeng_analytics.dart';
+
+typedef String PageNameGenerator(PageRoute<dynamic> pageRoute);
+
+String kDefaultPageNameGenerator(PageRoute<dynamic> pageRoute) =>
+    pageRoute.settings.name;
+
+/// A [Navigator] observer that send page view events to UMeng Analytics when
+/// [PageRoute] changes.
+///
+/// To track page views automatically, add this class to [Navigator.observers]
+/// or if you are using [MaterialApp], add this to
+/// [MaterialApp.navigatorObservers]:
+///
+/// ```dart
+/// new MaterialApp(
+///   navigatorObservers: [
+///     new UMengAnalyticsRouteObserver(),
+///   ],
+/// );
+/// ```
+///
+/// You can customize page name using `pageNameGenerator`, the related
+/// [RouteSettings.name] is used as page name by default.
+class UMengAnalyticsRouteObserver extends RouteObserver<PageRoute<dynamic>> {
+  final PageNameGenerator pageNameGenerator;
+
+  UMengAnalyticsRouteObserver({
+    this.pageNameGenerator = kDefaultPageNameGenerator,
+  });
+
+  @override
+  void didPush(Route route, Route previousRoute) {
+    super.didPush(route, previousRoute);
+    if (route is PageRoute) {
+      UMengAnalytics.beginPageView(pageNameGenerator(route));
+    }
+  }
+
+  @override
+  void didPop(Route route, Route previousRoute) {
+    super.didPop(route, previousRoute);
+    if (previousRoute is PageRoute && route is PageRoute) {
+      UMengAnalytics.endPageView(pageNameGenerator(previousRoute));
+    }
+  }
+}
